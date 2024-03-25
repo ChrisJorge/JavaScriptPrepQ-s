26e45e874c299cacff1381caa771489b495cbcf9
Chris <Christopherjorge47@icloud.com>
2024-03-25 13:49:38 -0400

    Added semi colons to first problem

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Write a program to capitalize first letter of each word in a string
 let capitalizeFirst = (string) => {
-    this.string = string 
-    let arr = string.split(" ")
-    let new_arr = []
+    this.string = string ;
+    let arr = string.split(" ");
+    let new_arr = [];
     for (let i = 0; i < arr.length; i++)
     {
         let txt = arr[i];
@@ -11,21 +11,24 @@ let capitalizeFirst = (string) => {
         {
             if(i === 0)
             {
-                newstr += txt[i].toUpperCase()
+                newstr += txt[i].toUpperCase();
             }
             else{
-                newstr += txt[i]
-            }
-        }
-        new_arr.push(newstr)
-    }
-    let returnstr = ''
+                newstr += txt[i];
+            };
+        };
+        new_arr.push(newstr);
+    };
+    let returnstr = '';
     for (let i = 0; i < new_arr.length; i++)
     {
-        returnstr += (new_arr[i] + ' ')
-    }
-    return returnstr
+        returnstr += (new_arr[i] + ' ');
+    };
+    return returnstr;
 }
 
+//  Reverse a given string
+let reverseString = (string) => {
 
+}
 console.log(capitalizeFirst('testing to see if works'));
