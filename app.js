68fff9d75c765a7d979014928a977a5e1400417a
Chris <Christopherjorge47@icloud.com>
2024-03-27 13:41:50 -0400

    Added input validation for positive_negative  problem

--- app.js
+++ app.js
@@ -114,6 +114,12 @@ let closestOnehundred = (num1 = 0, num2 = 0) =>
 let positive_negative = (num1, num2) => {
     this.num1 = num1;
     this.num2 = num2;
+
+    if(typeof(num1) !== 'number' || typeof(num2) !== 'number')
+    {
+        return 'Invalid input: Both inputs are not numbers'
+    }
+    
     if(num1 % 2 === 0 && num2 % 2 !== 0)
     {
         return true;
@@ -187,6 +193,7 @@ let numVowels = (str = '') => {
 let return50 = (num1 = 0, num2 = 0) => {
     this.num1 = num1;
     this.num2 = num2;
+
 
     if(num1 === 50 || num2 === 50)
     {
